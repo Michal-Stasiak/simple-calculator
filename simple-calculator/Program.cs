2595ef1539b7c1b912deb0c5a36cda2c859cdc83
Michał Stasiak <michal.stasiak@protonmail.com>
2022-07-03 17:01:08 +0200

    Add project files.

---
+++ simple-calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj pierwszą liczbę: ");
+                var num1 = GetInput();
+
+                Console.WriteLine("Jaką operację chcesz wykonać?\nMożliwe operacje to: '+', '-', '/', '*'");
+                var action = Console.ReadLine();
+
+                Console.WriteLine("Podaj drugą liczbę: ");
+                var num2 = GetInput();
+
+                var result = Calculate(num1, num2, action);
+
+                Console.WriteLine("Wynik twojego działania to: " + result);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static int GetInput()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+                throw new Exception("Podana wartość jest nieprawidłowa");
+
+            return input;
+        }
+        private static int Calculate(int num1, int num2, string action)
+        {
+            switch (action)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "/":
+                    return num1 / num2;
+                case "*":
+                    return num1 * num2;
+                default:
+                    throw new Exception("Wybrałeś złą operację!");
+            }
+        }
+    }
+}
